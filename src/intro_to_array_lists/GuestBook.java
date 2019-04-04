74a5a02f5eb143e8908443e83f6681b38471b9fb
league <league@wts-im-6.attlocal.net>
2019-04-03 19:30:40 -0700

    Intro To Array Lists and Guest Book.

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,63 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+//Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
+// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+// all the names added to the list. Format the list as follows:
+// Guest #1: Bob Banders
+// Guest #2: Sandy Summers
+// Guest #3: Greg Ganders
+// Guest #4: Donny Doners
+
+public class GuestBook implements ActionListener {
+
+	ArrayList<String> names = new ArrayList<String>();
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton("Add a Name");
+	JButton button2 = new JButton("View Names");
+
+	public static void main(String[] args) {
+		GuestBook guestBook = new GuestBook();
+		guestBook.run();
+
+	}
+
+	public void run() {
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+
+		panel.add(button1);
+		panel.add(button2);
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(300, 60);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == button1) {
+			names.add(JOptionPane.showInputDialog("Enter a name. "));
+		} else if (e.getSource() == button2) {
+			for (int i = 0; i < names.size(); i++) {
+				int a = i + 1;
+				System.out.println("Guest #" + a + ": " + names.get(i));
+			}
+
+			System.out.println("\n");
+
+		}
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,51 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
-		
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> string = new ArrayList<String>();
+
+		// 2. Add five Strings to your list
+		string.add("a");
+		string.add("b");
+		string.add("c");
+		string.add("d");
+		string.add("e");
+
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < string.size(); i++) {
+			System.out.println(string.get(i));
+		}
+
+		System.out.println("\n");
+		// 4. Print all the Strings using a for-each loop
+		for (String s : string) {
+			System.out.println(s);
+		}
+
+		System.out.println("\n");
+		// 5. Print only the even numbered elements in the list.
+		for (int i = 0; i < string.size(); i++) {
+			if (i % 2 == 1) {
+				System.out.println(string.get(i));
+			}
+		}
+
+		System.out.println("\n");
+		// 6. Print all the Strings in reverse order.
+		for (int i = string.size() - 1; i >= 0; i--) {
+			System.out.println(string.get(i));
+		}
+
+		System.out.println("\n");
+		// 7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < string.size(); i++) {
+			if (string.get(i).contains("e")) {
+				System.out.println(string.get(i));
+			}
+		}
 	}
 }
